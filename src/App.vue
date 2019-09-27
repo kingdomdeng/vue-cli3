3890a30b1a6fc3251206bb6d8880793b894a189b
kingdomdeng <kingdomdeng@163.com>
2019-09-27 09:40:22 +0800

    feat eventBus demo

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <img src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <test></test>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import test from './components/test.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    test
   }
 }
 </script>
---
+++ src/components/test.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="test">
+       Test component receive eventBus data: {{ test }}
+    </div>
+</template>
+
+<script>
+    import eventBus from "../utils/eventBus";
+
+    export default {
+        name: 'Test',
+        props: {
+            msg: String
+        },
+        data() {
+          return {
+              test: ""
+          };
+        },
+        mounted() {
+            eventBus.$on("eventBus", (data) => {
+                this.test = data;
+            });
+        },
+        methods: {
+
+        }
+    }
+</script>
+
