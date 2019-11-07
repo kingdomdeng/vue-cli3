04ceadc60e725c211c46c902b44aeac468f1cfec
kingdomdeng <kingdomdeng@163.com>
2019-11-07 12:57:15 +0800

    fix: 配置请求地址

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const APP_CONFIG = {
-  // baseURL: "http://localhost:8080"
-  baseURL: "https://health.sina.cn/c/api"
+  baseURL: "http://localhost:8080"
+  // baseURL: "https://health.sina.cn/c/api"
 };
